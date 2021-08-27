09e58aaca35f3c04dc55ac35bb90329c528f74dd
Wesley Matson <wesley@jelli.com>
2021-08-27 10:27:30 -0600

    Updating ui to default the base data

--- service/src/main/resources/static/ui/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { jelli.hackday_environ_manager.core.init(); } catch (e) { console.error("An error occurred when calling (jelli.hackday-environ-manager.core/init)"); throw(e); }
