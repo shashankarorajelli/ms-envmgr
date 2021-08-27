95f387481cc259842616b0bb25b3f47708bd5d84
Wesley Matson <wesley@jelli.com>
2021-08-27 09:54:00 -0600

    Removing extraneous folder

--- service/src/main/resources/static/ui/public/js/compiled/cljs-runtime/jelli.hackday_environ_manager.config.js
+++
@@ -1,4 +0,0 @@
-goog.provide('jelli.hackday_environ_manager.config');
-jelli.hackday_environ_manager.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=jelli.hackday_environ_manager.config.js.map
--- service/src/main/resources/static/ui/public/js/compiled/cljs-runtime/jelli.hackday_environ_manager.simple_interop.js
+++
@@ -1,9 +0,0 @@
-goog.provide('jelli.hackday_environ_manager.simple_interop');
-jelli.hackday_environ_manager.simple_interop.new_tab_href = (function jelli$hackday_environ_manager$simple_interop$new_tab_href(link){
-return new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"href","href",-793805698),link,new cljs.core.Keyword(null,"target","target",253001721),"_blank"], null);
-});
-jelli.hackday_environ_manager.simple_interop.event__GT_value = (function jelli$hackday_environ_manager$simple_interop$event__GT_value(e){
-return e.target.value;
-});
-
-//# sourceMappingURL=jelli.hackday_environ_manager.simple_interop.js.map
